e6a9741f4bde638a8b62fa280c28c4eef35f4114
TERBOUCHE Hacene <hacene.terbouche@gmail.com>
2024-05-08 11:19:50 +0200

    add goe constants

---
+++ src/svl/utils/constants.py
@@ -0,0 +1,12 @@
+import math
+
+EARTH_RADIUS = 6371.0  # km
+EARTH_CIRCUMFERENCE = 2 * math.pi * EARTH_RADIUS  # km
+EQUTORIAL_CIRCUMFERENCE_METERS = 40075016.686  # meters
+TILE_SIZE = 256  # pixels
+MAX_ZOOM_LEVEL = 20  # maximum zoom level
+MIN_ZOOM_LEVEL = 1  # minimum zoom level
+MAX_LATITUDE = 90  # degrees
+MIN_LATITUDE = -90  # degrees
+MAX_LONGITUDE = 180  # degrees
+MIN_LONGITUDE = -180  # degrees
